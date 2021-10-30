ae0dc84fa9c8a3aba62906cffbb9e7f2812f4351
ermakovss <60349645+ermakovss@users.noreply.github.com>
2021-10-30 16:34:15 +0400

    add constrains

--- Scripts/webcum.js
+++ Scripts/webcum.js
@@ -1,3 +1,21 @@
+const constraints = {
+    video: {
+        width: {
+            min: 1280,
+            ideal: 1920,
+            max: 2560,
+        },
+        height: {
+            min: 720,
+            ideal: 1080,
+            max: 1440
+        },
+        facingMode:{
+            exact: 'environment'
+        }
+    }
+};
+
 async function getPhoneWebCum(){
     try
     {
