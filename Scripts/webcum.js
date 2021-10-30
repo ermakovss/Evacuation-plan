b3a852abf3f1821006f29c36a8f1d8db5ca121f3
ermakovss <60349645+ermakovss@users.noreply.github.com>
2021-10-30 17:14:26 +0400

    Add files via upload

--- Scripts/webcum.js
+++ Scripts/webcum.js
@@ -1,20 +1,3 @@
-/*async function getPhoneWebCum(){
-    try
-    {
-        let videoSrc = await navigator.mediaDevices.getUserMedia({video:true});
-        let video = document.getElementById("video");
-        video.srcObject = videoSrc;
-    }
-    catch (e)
-    {
-        console.log(e);
-    }
-}
-
-getPhoneWebCum();*/
-const video = document.querySelector('video');
-const cameraOptions = document.querySelector('play');
-
 const constraints = {
     video: {
         width: {
@@ -31,20 +14,12 @@ const constraints = {
             exact: 'environment'
         }
     }
-};
-
-cameraOptions.onchange = () => {
-    const updatedConstraints = {
-        ...constraints
-    };
-
-    getPhoneWebCum(updatedConstraints);
 };
 
 async function getPhoneWebCum(){
     try
     {
-        let videoSrc = await navigator.mediaDevices.getUserMedia({video:true});
+        let videoSrc = await navigator.mediaDevices.getUserMedia(constraints);
         let video = document.getElementById("video");
         video.srcObject = videoSrc;
     }
@@ -54,97 +29,5 @@ async function getPhoneWebCum(){
     }
 }
 
-/*
-const controls = document.querySelector('.controls');
-const cameraOptions = document.querySelector('.video-options>select');
-const video = document.querySelector('video');
-const canvas = document.querySelector('canvas');
-const screenshotImage = document.querySelector('img');
-const buttons = [...controls.querySelectorAll('button')];
-let streamStarted = false;
+getPhoneWebCum();
 
-const [play, pause, screenshot] = buttons;
-
-const constraints = {
-    video: {
-        width: {
-            min: 1280,
-            ideal: 1920,
-            max: 2560,
-        },
-        height: {
-            min: 720,
-            ideal: 1080,
-            max: 1440
-        },
-        facingMode:{
-            exact: 'environment'
-        }
-    }
-};
-
-cameraOptions.onchange = () => {
-    const updatedConstraints = {
-        ...constraints,
-        deviceId: {
-            exact: cameraOptions.value
-        }
-    };
-
-    startStream(updatedConstraints);
-};
-
-const startStream = async (constraints) => {
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    handleStream(stream);
-};
-
-
-play.onclick = () => {
-    if (streamStarted) {
-        video.play();
-        play.classList.add('d-none');
-        pause.classList.remove('d-none');
-        return;
-    }
-    if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-        const updatedConstraints = {
-            ...constraints,
-            deviceId: {
-                exact: cameraOptions.value
-            }
-        };
-        startStream(updatedConstraints);
-    }
-};
-
-const pauseStream = () => {
-    video.pause();
-    play.classList.remove('d-none');
-    pause.classList.add('d-none');
-};
-
-const doScreenshot = () => {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext('2d').drawImage(video, 0, 0);
-    screenshotImage.src = canvas.toDataURL('image/webp');
-    screenshotImage.classList.remove('d-none');
-};
-
-pause.onclick = pauseStream;
-screenshot.onclick = doScreenshot;
-
-const startStream = async (constraints) => {
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    handleStream(stream);
-};
-
-
-const handleStream = (stream) => {
-    video.srcObject = stream;
-    play.classList.add('d-none');
-    pause.classList.remove('d-none');
-    screenshot.classList.remove('d-none');
-};
-*/
