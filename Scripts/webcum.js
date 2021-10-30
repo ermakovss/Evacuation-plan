32224dfc34fdf9b3b705ce844d39d103771bd6a8
ermakovss <60349645+ermakovss@users.noreply.github.com>
2021-10-30 17:05:20 +0400

    Add files via upload

--- Scripts/webcum.js
+++ Scripts/webcum.js
@@ -12,7 +12,52 @@
 }
 
 getPhoneWebCum();*/
+const video = document.querySelector('video');
+const cameraOptions = document.querySelector('.video-options>select');
 
+const constraints = {
+    video: {
+        width: {
+            min: 1280,
+            ideal: 1920,
+            max: 2560,
+        },
+        height: {
+            min: 720,
+            ideal: 1080,
+            max: 1440
+        },
+        facingMode:{
+            exact: 'environment'
+        }
+    }
+};
+
+cameraOptions.onchange = () => {
+    const updatedConstraints = {
+        ...constraints,
+        deviceId: {
+            exact: cameraOptions.value
+        }
+    };
+
+    getPhoneWebCum(updatedConstraints);
+};
+
+async function getPhoneWebCum(){
+    try
+    {
+        let videoSrc = await navigator.mediaDevices.getUserMedia({video:true});
+        let video = document.getElementById("video");
+        video.srcObject = videoSrc;
+    }
+    catch (e)
+    {
+        console.log(e);
+    }
+}
+
+/*
 const controls = document.querySelector('.controls');
 const cameraOptions = document.querySelector('.video-options>select');
 const video = document.querySelector('video');
@@ -51,6 +96,12 @@ cameraOptions.onchange = () => {
 
     startStream(updatedConstraints);
 };
+
+const startStream = async (constraints) => {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleStream(stream);
+};
+
 
 play.onclick = () => {
     if (streamStarted) {
@@ -98,17 +149,5 @@ const handleStream = (stream) => {
     play.classList.add('d-none');
     pause.classList.remove('d-none');
     screenshot.classList.remove('d-none');
-
 };
-
-
-const getCameraSelection = async () => {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter(device => device.kind === 'videoinput');
-    const options = videoDevices.map(videoDevice => {
-        return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
-    });
-    cameraOptions.innerHTML = options.join('');
-};
-
-getCameraSelection();
+*/
